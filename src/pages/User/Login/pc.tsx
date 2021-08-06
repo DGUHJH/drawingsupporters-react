b6e6d34f96efe8551ddd8fdd0eda185662876ece
DGUHJH <ghkdwogud852@gmail.com>
2021-08-06 14:11:02 +0900

    Add : Login

--- src/pages/User/Login/pc.tsx
+++ src/pages/User/Login/pc.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import * as Styled from './styled';
 
+const height = 80;
+const fontSize = 24;
+const InputLabelProps = {
+	style: {
+		fontFamily: 'Noto Sans Regular',
+		height,
+		fontSize,
+	},
+};
+const inputProps = {
+	style: {
+		fontFamily: 'Noto Sans Regular',
+		height,
+		fontSize,
+		padding: '0 24px',
+	},
+};
+
 const UserLogin = () => {
-	return <Styled.Root>asd</Styled.Root>;
+	return (
+		<Styled.Root>
+			<Styled.LoginContainer>
+				<Styled.LoginTitleTypo>
+					간편하게 로그인하고 <br />
+					비대면 코칭을 경험해보세요.
+				</Styled.LoginTitleTypo>
+				<Styled.RequestEditor
+					style={{ height, marginTop: '80px' }}
+					placeholder="아이디 (이메일 주소)"
+					variant="outlined"
+					InputLabelProps={InputLabelProps}
+					inputProps={inputProps}
+				/>
+				<Styled.RequestEditor
+					style={{ height }}
+					placeholder="비밀번호"
+					type="password"
+					variant="outlined"
+					InputLabelProps={InputLabelProps}
+					inputProps={inputProps}
+				/>
+				<Styled.LoginSubmitButton>
+					<Styled.LoginSubmitButtonTypo>로그인</Styled.LoginSubmitButtonTypo>
+				</Styled.LoginSubmitButton>
+				<Styled.JoinTypo>비밀번호 찾기 ㅣ 회원가입</Styled.JoinTypo>
+			</Styled.LoginContainer>
+		</Styled.Root>
+	);
 };
 
 export default UserLogin;
--- src/pages/User/Login/styled.ts
+++ src/pages/User/Login/styled.ts
@@ -1,6 +1,67 @@
-import { Box } from '@material-ui/core';
+import { Box, Button, TextField, Typography } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const Root = styled(Box)`
 	width: 100%;
+	display: flex;
+	justify-content: center;
+	margin-top: 100px;
+	margin-bottom: 500px;
+`;
+
+export const LoginContainer = styled(Box)`
+	width: 800px;
+	height: 680px;
+	border: 1px #e4e4e4 solid;
+	border-radius: 8px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+export const LoginTitleTypo = styled(Typography)`
+	&&& {
+		font-family: 'Noto Sans Regular';
+		font-weight: bold;
+		font-size: 32px;
+		display: flex;
+		text-align: center;
+		margin-top: 60px;
+	}
+`;
+
+export const RequestEditor = styled(TextField)`
+	&&& {
+		width: 628px;
+		height: 80px;
+		margin-bottom: 20px;
+	}
+`;
+
+export const LoginSubmitButton = styled(Button)`
+	&&& {
+		width: 628px;
+		height: 80px;
+		background: #248bd7;
+		margin-top: 80px;
+		border-radius: 8px;
+	}
+`;
+
+export const LoginSubmitButtonTypo = styled(Typography)`
+	&&& {
+		font-family: 'Noto Sans Regular';
+		font-weight: bold;
+		font-size: 24px;
+		color: white;
+	}
+`;
+
+export const JoinTypo = styled(Typography)`
+	&&& {
+		font-family: 'Noto Sans Regular';
+		font-size: 24px;
+		color: #7e7e7e;
+		margin-top: 20px;
+	}
 `;

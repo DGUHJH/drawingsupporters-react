3a6dd31baf1480282990c5d68d862791ac7673bc
DGUHJH <ghkdwogud852@gmail.com>
2021-08-22 13:39:13 +0900

    Update : tablet view

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -14,62 +14,62 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component<{}, { width: number }> {
-	constructor(props: any) {
-		super(props);
-		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-		this.state = { width: 1000 };
-	}
+  constructor(props: any) {
+    super(props);
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.state = { width: 1000 };
+  }
 
-	componentDidMount() {
-		this.updateWindowDimensions();
-		window.addEventListener('resize', this.updateWindowDimensions);
-	}
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
 
-	componentWillUnmount() {
-		window.removeEventListener('resize', this.updateWindowDimensions);
-	}
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
 
-	updateWindowDimensions() {
-		this.setState({ width: window.innerWidth });
-	}
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
 
-	render() {
-		return (
-			<Box>
-				{this.state.width > 1000 ? (
-					<>
-						<Header />
-						<Switch>
-							<Route exact path="/feedback/feedback/:id" component={Feedback} />
-							<Route exact path="/user/login" component={UserLogin} />
-							<Route exact path="/user/join" component={UserJoin} />
-							<Route exact path="/user/mentor-join" component={MentorJoin} />
-							<Route
-								exact
-								path="/feedback/request"
-								component={FeedbackRequest}
-							/>
-							<Route exact path="/feedback/list" component={FeedbackList} />
-							<Route
-								exact
-								path="/feedback/details/:id"
-								component={FeedbackDetails}
-							/>
-							<Route
-								exact
-								path="/request/details/:id"
-								component={RequestDetails}
-							/>
-							<Route exact path="/" component={Main} />
-						</Switch>
-						<Footer />
-					</>
-				) : (
-					<Box>모바일은 지원하지 않습니다!</Box>
-				)}
-			</Box>
-		);
-	}
+  render() {
+    return (
+      <Box>
+        {true ? (
+          <>
+            <Header />
+            <Switch>
+              <Route exact path="/feedback/feedback/:id" component={Feedback} />
+              <Route exact path="/user/login" component={UserLogin} />
+              <Route exact path="/user/join" component={UserJoin} />
+              <Route exact path="/user/mentor-join" component={MentorJoin} />
+              <Route
+                exact
+                path="/feedback/request"
+                component={FeedbackRequest}
+              />
+              <Route exact path="/feedback/list" component={FeedbackList} />
+              <Route
+                exact
+                path="/feedback/details/:id"
+                component={FeedbackDetails}
+              />
+              <Route
+                exact
+                path="/request/details/:id"
+                component={RequestDetails}
+              />
+              <Route exact path="/" component={Main} />
+            </Switch>
+            <Footer />
+          </>
+        ) : (
+          <Box>모바일은 지원하지 않습니다!</Box>
+        )}
+      </Box>
+    );
+  }
 }
 
 export default App;
